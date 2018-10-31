947b42c93adac95723135ee834ee76441fb798df
Kenneth Vittetoe <kenneth.vittetoe@ujcv.edu.hn>
2018-10-31 08:09:06 -0600

    implementando resta

--- src/hn/edu/hn/edu/ujcv/lib/al/Mat2x2.java
+++ src/hn/edu/hn/edu/ujcv/lib/al/Mat2x2.java
@@ -105,7 +105,10 @@ public class Mat2x2 {
         //   cy es colY
         //   x es   getX , setX
         //   y es   getY , setY
-
+        retval.colX.setX(this.colX.getX() - b.colX.getX());
+        retval.colX.setY(this.colX.getY() - b.colX.getY());
+        retval.colY.setX(this.colY.getX() - b.colY.getX());
+        retval.colY.setY(this.colY.getY() + b.colY.getY());
         return retval;
     }
     // multiplicacion escalar
