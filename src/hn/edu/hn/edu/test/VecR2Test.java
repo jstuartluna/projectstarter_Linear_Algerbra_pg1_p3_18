e8dcc27e28877fcc367eded2219f8fef2c719873
Kenneth Vittetoe <kenneth.vittetoe@ujcv.edu.hn>
2018-10-31 20:33:27 -0600

    Agregando unit test para pruebas
    Signed-off-by: Kenneth Vittetoe <kenneth.vittetoe@ujcv.edu.hn>

---
+++ src/hn/edu/hn/edu/test/VecR2Test.java
@@ -0,0 +1,78 @@
+package hn.edu.hn.edu.test;
+
+import hn.edu.hn.edu.ujcv.lib.al.VecR2;
+
+
+import static org.junit.Assert.*;
+
+public class VecR2Test {
+    private VecR2 a;
+    private VecR2 b;
+    private VecR2 c;
+    private VecR2 d;
+    private VecR2 e;
+    private VecR2 f;
+    private double delta;
+
+    @org.junit.Before
+    public void setUp() throws Exception {
+        a = new VecR2();// constructor por defecto.
+        b = new VecR2(2.2,1.1); //constructor de conveniencia.
+        c = new VecR2(b); // constructor de copia.
+        d = new VecR2(-2.2,-1.1);
+        e = new VecR2(3,3);
+        f = new VecR2(-10,50);
+        delta = 0.00001;
+
+    }
+
+    @org.junit.After
+    public void tearDown() throws Exception {
+    }
+
+    @org.junit.Test
+    public void add() {
+        VecR2 temp = a.add(b);
+        assertEquals(b.getX(),temp.getX(),delta);
+        assertEquals(b.getY(),temp.getY(),delta);
+        temp = b.add(d);
+        assertEquals(a.getX(),temp.getX(),delta);
+        assertEquals(a.getY(),temp.getY(),delta);
+        temp = f.add(f);
+        assertEquals(f.getX()*2,temp.getX(),delta);
+        assertEquals(f.getY()*2,temp.getY(),delta);
+
+    }
+
+    @org.junit.Test
+    public void sub() {
+    }
+
+    @org.junit.Test
+    public void mul() {
+    }
+
+    @org.junit.Test
+    public void dotProduct() {
+    }
+
+    @org.junit.Test
+    public void div() {
+    }
+
+    @org.junit.Test
+    public void getX() {
+    }
+
+    @org.junit.Test
+    public void setX() {
+    }
+
+    @org.junit.Test
+    public void getY() {
+    }
+
+    @org.junit.Test
+    public void setY() {
+    }
+}
---
+++ src/hn/edu/hn/edu/test/VecR3Test.java
@@ -0,0 +1,42 @@
+package hn.edu.hn.edu.test;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class VecR3Test {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void add() {
+    }
+
+    @Test
+    public void sub() {
+    }
+
+    @Test
+    public void mul() {
+    }
+
+    @Test
+    public void dotProduct() {
+    }
+
+    @Test
+    public void crossProduct() {
+    }
+
+    @Test
+    public void div() {
+    }
+}
