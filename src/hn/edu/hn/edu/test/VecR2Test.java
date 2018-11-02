4b6a80050ac3716b4adf0e84acbed2f198aed620
Kenneth Vittetoe <kenneth.vittetoe@ujcv.edu.hn>
2018-11-02 07:57:46 -0600

    pureba Vector r2 completa.
    Signed-off-by: Kenneth Vittetoe <kenneth.vittetoe@ujcv.edu.hn>

--- src/hn/edu/hn/edu/test/VecR2Test.java
+++ src/hn/edu/hn/edu/test/VecR2Test.java
@@ -14,6 +14,11 @@ public class VecR2Test {
     private VecR2 f;
     private VecR2 fx2;
     private double delta;
+    private VecR2 ex3;
+    private double aDb;
+    private double bDd;
+    private double eDe;
+    private double dDe;
 
     @org.junit.Before
     public void setUp() throws Exception {
@@ -24,6 +29,12 @@ public class VecR2Test {
         e = new VecR2(3,3);
         f = new VecR2(-10,50);
         fx2 = new VecR2(-20,100);
+        ex3 = new VecR2(9,9);
+        aDb = 0;
+        bDd = -4.84 + -1.21;
+        eDe = 3*3 +3*3;
+        dDe = -2.2 * 3 + -1.1 * 3;
+
         delta = 0.00001;
 
     }
@@ -47,20 +58,39 @@ public class VecR2Test {
 
     @org.junit.Test
     public void sub() {
+        VecR2 temp = a.add(b);
+        assertEqualsVecR2(b,temp,delta);
+        temp = b.add(d);
+        assertEqualsVecR2(a,temp,delta);
+        temp = f.add(f);
+        assertEqualsVecR2(fx2,temp,delta);
     }
 
     @org.junit.Test
     public void mul() {
+        VecR2 temp = e.mul(3);
+        assertEqualsVecR2(ex3,temp,delta);
+        temp = f.mul(2);
+        assertEqualsVecR2(fx2,temp,delta);
     }
 
     @org.junit.Test
     public void dotProduct() {
+        assertEquals(aDb,a.dotProduct(b),delta);
+        assertEquals(bDd,b.dotProduct(d),delta);
+        assertEquals(eDe,e.dotProduct(e),delta);
+        assertEquals(dDe,d.dotProduct(e),delta);
     }
 
     @org.junit.Test
     public void div() {
+        VecR2 temp = e.div(3);
+        assertEqualsVecR2(new VecR2(1,1),temp,delta);
+        temp = ex3.div(3);
+        assertEqualsVecR2(e,temp,delta);
+        temp = fx2.div(2.00000000000001);
+        assertEqualsVecR2(f,temp,delta);
     }
-
     public static void assertEqualsVecR2(VecR2 expected, VecR2 actual, double delta){
         assertEquals(expected.getX(),actual.getX(),delta);
         assertEquals(expected.getY(),actual.getY(),delta);
